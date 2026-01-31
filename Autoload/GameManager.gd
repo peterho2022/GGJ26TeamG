140a4915c50cdafe466ba20dcf0596ddca1d8143
Bread <a0900518266@gmail.com>
2026-01-31 16:04:57 +0800

    修

--- Autoload/GameManager.gd
+++ Autoload/GameManager.gd
@@ -59,7 +59,6 @@ func _go_to_end_scene():
 func _change_scene_safe(path: String):
 	print("正在模擬切換到場景: ", path) 
 	get_tree().change_scene_to_file(path)
-<<<<<<< HEAD
 	
 func load_specific_level(index: int):
 	# 安全檢查：確保 index 沒有超出陣列範圍
@@ -68,11 +67,9 @@ func load_specific_level(index: int):
 		_change_scene_safe(level_list[index]) # 載入該關卡
 	else:
 		print("錯誤：關卡索引超出範圍！")
-=======
 
 func add_tape_length(v: float):
 	total_tape_length += v
 	
 func reset_tape_length(v: float):
 	total_tape_length = 0
->>>>>>> 4a9e9b6899370d4883c1afa7e9c34146803a9ceb
