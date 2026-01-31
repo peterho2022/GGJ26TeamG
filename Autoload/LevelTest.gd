d9790d9adb29bcb92086a9edfbc1d99b0be48b6b
Bread <a0900518266@gmail.com>
2026-01-31 18:39:03 +0800

    上傳end scene的結尾評價跟膠帶數量(假的)

--- Autoload/LevelTest.gd
+++ Autoload/LevelTest.gd
@@ -1,11 +1,10 @@
 extends Node
 
 # 取得 CanvasLayer 下的 Label
-@onready var debug_label = $CanvasLayer/DebugLabel
+# @onready var debug_label = $CanvasLayer/DebugLabel
 
 func _ready():
 	print("載入成功！目前關卡索引：", GameManager.current_level_index)
-	update_debug_display()
 
 func _input(event):
 	# 只有在按鈕 "剛按下" 的那一瞬間觸發 (避免按住不放一直觸發)
@@ -22,19 +21,8 @@ func _input(event):
 	elif event is InputEventKey and event.keycode == KEY_T:
 		GameManager.add_tape_length(3.0) # 統一增加 3.0
 		print("作弊成功！增加 3.0m")
-		update_debug_display()
 		
 	# --- 3. 重置分數 (R 鍵) ---
 	elif event is InputEventKey and event.keycode == KEY_R:
 		GameManager.reset_tape_length() 
 		print("重置分數")
-		update_debug_display()
-
-# 更新畫面顯示的小工具
-func update_debug_display():
-	if debug_label:
-		debug_label.text = "目前累積模擬膠帶: " + str(GameManager.total_tape_length) + " m"
-		
-		# 顯示評價預測
-		var temp_rank = GameManager.calculate_rank()
-		debug_label.text += "\n當前預估評價: " + temp_rank
--- Scenes/end.gd
+++ Scenes/end.gd
@@ -1,8 +1,25 @@
 extends Node
 
-func _ready():
-	print("請按 [Space] 鍵回到主選單...")
 
-func _on_button_pressed() -> void:
-	print("按鈕：回到主選單")
+@onready var score_label = $Background/VBoxContainer2/ScoreLabel
+@onready var rank_label = $Background/VBoxContainer2/RankLabel	
+
+func _ready():
+	
+	# 1. 顯示數字
+	var final_score = GameManager.total_tape_length
+	score_label.text = "最終膠帶使用量: " + str(final_score) + " m"
+	
+	# 2. 顯示評價
+	var final_rank = GameManager.calculate_rank()
+	rank_label.text = "獲得評價: " + final_rank
+	
+	# (選用) 根據評價變色
+	if final_rank == "S":
+		rank_label.modulate = Color.GOLD
+	elif final_rank == "D":
+		rank_label.modulate = Color.GRAY
+
+# --- 按鈕功能 ---
+func _on_button_pressed():
 	GameManager.back_to_menu()
